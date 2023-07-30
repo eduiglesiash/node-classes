1559fc69a186dd6454be40222d1a151e62ba89da
Edu Iglesias <eiglesiash@gmail.com>
2023-07-30 10:29:43 +0200

    Url creadas con Node

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -6,10 +6,10 @@ const desiredPort = process.env.PORT ?? 1234
 const processRequest = (req, res)=>{
   res.setHeader('Content-type', 'text/html; charset=utf-8')
   if (req.url === '/'){
-    res.statusCode = 200 // ok
+    res.statusCode = 200  // ok
     res.end('<h1>Bienvenido a mi página de incio</h1>')
   } else if(req.url === '/contacto' ){
-    res.statusCode = 200;
+    res.statusCode = 200; // ok ∫
     res.end('<h1> Contacto </h1>')
   } else {
     res.statusCode = 404 // not found
